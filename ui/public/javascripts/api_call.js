ee519a9f1ce4336fcc5b05edc92357be519b7d1a
Emanuel Turtula <emanuelturtula@gmail.com>
2021-07-03 21:50:26 -0300

    Finished ui

--- ui/public/javascripts/api_call.js
+++ ui/public/javascripts/api_call.js
@@ -1,9 +1,13 @@
 let url = 'http://192.168.0.2:3000/horno/';
 
-fetchText();
+var t = setInterval(fetchData,1000);
 
-async function fetchText() {
+async function fetchData() {
     let response = await fetch(url, {mode: 'cors'});
     let data = await response.text();
-    console.log(data);
+    let parsedData = JSON.parse(data);
+    console.log(parsedData);
+    var temp = document.getElementById("temp");
+    temp.innerHTML = parsedData.temperature
 }
+
